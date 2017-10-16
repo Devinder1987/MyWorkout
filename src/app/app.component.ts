8f7720c1febbdbfe9053a6f8217144ace545825e
Devinder <dsuthwal@xebia.com>
2017-10-16 12:44:21 +0530

    raushan changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-// App Component
+// App Component raushan
 import { TabsPage } from '../pages/tabs/tabs';
 @Component({
   templateUrl: 'app.html'
